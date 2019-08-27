6aca979def83f0bfc787efbcb7ab57bb2ac0c9c8
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2019-08-28 01:01:02 +0530

    all features are working according kernel

--- main.py
+++ main.py
@@ -37,24 +37,25 @@ df_positive.head()
 
 k= (' '.join(df_positive['comment_text']))
 
-wordcloud = WordCloud(width = 1000, height = 500).generate(k)
+'''wordcloud = WordCloud(width = 1000, height = 500).generate(k)
 plt.figure(figsize=(15,5))
-plt.imshow(wordcloud)
-plt.axis('off')
+plt.show(wordcloud)
+plt.axis('off')'''
 
 
-df_negative = comm[comm.pol==-1]
+'''df_negative = comm[comm.pol==-1]
 k= (' '.join(df_negative['comment_text']))
 wordcloud = WordCloud(width = 1000, height = 500).generate(k)
 plt.figure(figsize=(15,5))
 plt.imshow(wordcloud)
-plt.axis('off')
+plt.axis('off')'''
 
 
 comm['pol'].replace({1:'positive',0:'Neutral',-1:'negative'}).value_counts().plot(kind='bar',figsize=(7,4))
 plt.title('Number of types of commets')
 plt.xlabel('Comment_type')
 plt.ylabel('number')
+plt.show()
 
 id=[]
 pos_comm=[]
@@ -107,13 +108,13 @@ for i in most_trending:
 tags = videos['tags'].map(lambda k: k.lower().split('|')).values
 
 # joining and making a complete list
-k= (' '.join(videos['tags']))
+'''k= (' '.join(videos['tags']))
 wordcloud = WordCloud(width = 1000, height = 500).generate((' '.join(k.lower().split('|'))))# word cloud
 
 
 plt.figure(figsize=(15,5))
 plt.imshow(wordcloud)
-plt.axis('off')
+plt.axis('off')'''
 
 videos.columns
 
@@ -124,21 +125,25 @@ df1.head(6)
 df_channel =pd.DataFrame(videos.groupby(by=['channel_title'])['views'].mean()).sort_values(by='views',ascending=False)
 df_channel.head(10).plot(kind='bar')
 plt.title('Most viewed channels')
+plt.show()
 
 
 df_channel =pd.DataFrame(videos.groupby(by=['channel_title'])['likes'].mean()).sort_values(by='likes',ascending=False)
 df_channel.head(10).plot(kind='bar')
 plt.title('Most liked channels')
+plt.show()
 
 videos['likes_per_view']=videos['likes']/videos['views']
 df_channel =pd.DataFrame(videos.groupby(by=['channel_title'])['likes_per_view'].mean()).sort_values(by='likes_per_view',ascending=False)
 df_channel.head(10).plot(kind='bar')
 plt.title('Most liked channels')
+plt.show()
 
 videos['dislikes_per_view']=videos['dislikes']/videos['views']
 df_channel =pd.DataFrame(videos.groupby(by=['channel_title'])['dislikes_per_view'].mean()).sort_values(by='dislikes_per_view',ascending=False)
 df_channel.head(10).plot(kind='bar')
 plt.title('Most disliked channels')
+plt.show()
 
 
 unique = pd.read_csv('unique.csv',)
@@ -151,21 +156,28 @@ videos[videos.video_id == 'eERPlIdPJtI'].title[225]
 sns.barplot(data=unique.sort_values(by='pos_comm',ascending=False).head(10),x='id',y='pos_comm')
 plt.xticks(rotation=45)
 plt.figure(figsize=(5,4))
+plt.show()
 
 sns.barplot(data=unique.sort_values(by='neg_comm',ascending=False).head(10),x='id',y='neg_comm')
 plt.xticks(rotation=45)
 plt.figure(figsize=(5,4))
+plt.show()
 
 sns.barplot(data=unique.sort_values(by='total_comments',ascending=False).head(10),x='id',y='total_comments')
 plt.xticks(rotation=45)
 plt.figure(figsize=(5,4))
+plt.show()
 
 sns.regplot(data=videos,x='views',y='likes')
 plt.title("Regression plot for likes & views")
+plt.show()
 
 sns.regplot(data=videos,x='views',y='dislikes')
 plt.title("Regression plot for dislikes & views")
+plt.show()
+
 
 df_corr = videos[['views','likes','dislikes']]
 
 sns.heatmap(df_corr.corr(),annot=True)
+plt.show()
